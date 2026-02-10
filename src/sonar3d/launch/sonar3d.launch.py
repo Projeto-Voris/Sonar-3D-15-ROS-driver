27165ffa2cd0526d59ef6bac61276d03ca945bd8
daniel <daniel_regner@hotmail.com>
2026-02-10 13:30:31 -0300

    docs: add launch arguments

--- src/sonar3d/launch/sonar3d.launch.py
+++ src/sonar3d/launch/sonar3d.launch.py
@@ -1,16 +1,22 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument('namespace', default_value='', description='Namespace for the sonar3d node'),
+        DeclareLaunchArgument('ip', default_value='192.168.194.96', description='IP address of the sonar device'),
+        DeclareLaunchArgument('speed_of_sound', default_value='1491', description='Speed of sound in water in m/s'),
+
         Node(
             package='sonar3d',
             executable='sonar_publisher',
             name='sonar_node',
+            namespace=LaunchConfiguration('namespace'),
             output='screen',
             parameters=[
-                {'IP': '192.168.194.96'},  # Change to your sonar IP, '192.168.194.96' is the fallback ip.
-                {'speed_of_sound': 1491}
+                {'IP': LaunchConfiguration('ip')},
+                {'speed_of_sound': LaunchConfiguration('speed_of_sound')}
             ]
         )
     ])
